45ba762cd1c57ba15310d8316a4b7b57c0a8a1e1
DominicDev <s0l0estudi4r@gmail.com>
2025-05-01 15:22:57 +0200

    v0.0.2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-![n1](preview/n1.avif)
+[![Foto Preview](preview/n73.avif)](https://20essentials.github.io/project-000-073)
+
+<div align="center" style="display: flex; justify-content: center;">
+  <a  href="https://github.com/20essentials/project-000-072" target="_blank">&#8592;</a>
+  &nbsp;&nbsp;
+  <a  href="https://github.com/20essentials/project-000-074" target="_blank">&#8594;</a>
+</div>
--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-console.groupCollapsed("Reference");
-console.log(
-  "Manzdev youtube video: https://youtu.be/NXiFWpTB2Ug?si=zI6rOTdE94YFUGIp "
-);
-console.groupEnd();
-
-const COLORS = ["#fff2", "#fff4", "#fff7", "#fffc"];
+const COLORS = ['#fff2', '#fff4', '#fff7', '#fffc'];
 
 const generateStars = (totalStars, selector, size, duration) => {
   const shadowPositions = [];
@@ -19,11 +13,11 @@ const generateStars = (totalStars, selector, size, duration) => {
   }
 
   const star = document.querySelector(selector);
-  star.style.setProperty("--clon-shadow", shadowPositions.join(","));
-  star.style.setProperty("--size", size);
-  star.style.setProperty("--duration", duration);
+  star.style.setProperty('--clon-shadow', shadowPositions.join(','));
+  star.style.setProperty('--size', size);
+  star.style.setProperty('--duration', duration);
 };
 
-generateStars(200, ".star-1", "2px", "15s");
-generateStars(100, ".star-2", "3px", "20s");
-generateStars(50, ".star-3", "4px", "18s");
+generateStars(200, '.star-1', '2px', '15s');
+generateStars(100, '.star-2', '3px', '20s');
+generateStars(50, '.star-3', '4px', '18s');
